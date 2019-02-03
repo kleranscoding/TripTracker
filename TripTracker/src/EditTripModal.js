21c766361aec224e9c78c3f9a9d2817c41acd1df
kleranscoding <clarencecc88@gmail.com>
2019-02-02 22:10:22 -0600

    users can add/remove fav (fixed)

---
+++ TripTracker/src/EditTripModal.js
@@ -0,0 +1,201 @@
+import React, { Component } from 'react';
+import { AsyncStorage, StyleSheet, View, Text, ScrollView, 
+    TouchableHighlight, Alert, TouchableOpacity, Picker, Dimensions, } from 'react-native';
+import { Appbar, Button, TextInput, } from  'react-native-paper';
+import ModalDatePicker from 'react-native-datepicker-modal';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import CalendarPicker from 'react-native-calendar-picker';
+
+import { serverURL, tokenName, regexWhitespaceOnly, } from './config/envConst';
+
+
+/**
+ * STYLESHEETS
+ */
+
+const styles = StyleSheet.create({
+    appbarHeader:{
+        backgroundColor: 'rgb(36,152,219)',
+    },
+    contentTitle: {
+        fontSize: 24, fontFamily: 'Avenir',
+    },
+})
+
+const modalStyles = StyleSheet.create({
+    newLocGreeting: {
+        textAlign: 'center', 
+        fontSize: 24, fontFamily: 'Avenir', color: 'rgb(255,255,255)',
+    },
+    closeModalText: {
+        marginRight: 5, 
+        textAlign: 'right', fontSize: 18, color: 'rgb(255,255,255)',
+    },
+    modalHeader: {
+        marginBottom: 20, 
+        paddingBottom: 20, paddingTop: 35, backgroundColor: 'rgb(36,152,219)' 
+    },
+    datepicker: {
+        justifyContent: 'flex-start', flexDirection: 'row', alignItems: 'center',
+        borderColor: 'silver', borderWidth: 1, margin: 10,
+    },
+    "create_btn": {
+        marginTop: 25, marginLeft:'25%', 
+        borderRadius: 25,
+        backgroundColor: 'rgba(36,152,219,0.75)',
+        width: '50%',
+    },
+    "create_btn_text": {
+        padding: 10, 
+        textAlign: 'center', fontSize: 20, color: 'rgb(255,255,255)'
+    },
+})
+
+/**
+ * FUNCTIONS AND CONSTANTs 
+ */
+function validateWhtieSpaceOnly(text) { return regexWhitespaceOnly.test(text) }
+
+function padZero(num) { return (num>=0 && num<=9)? `0${num}`:`${num}`; }
+
+function dateToString(selectedDate) {
+    let date= new Date(selectedDate)
+    return `${date.getFullYear()}-${padZero(date.getMonth()+1)}-${padZero(date.getDate())}`
+}
+
+_getToken = async() => { return await AsyncStorage.getItem(tokenName) }
+
+
+export default class EditTripModal extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tripTitle: props.selectOnEdit.title,
+            dateStart: props.selectOnEdit.startDate, 
+            dateEnd: props.selectOnEdit.endDate,
+            errTripTitle: false,
+        }
+    }
+
+    submitEditTripInfo = () => {
+        let title = this.state.tripTitle
+        let startDate = this.state.dateStart, endDate = this.state.dateEnd
+        
+        // check errors
+        let errTitle= true, errDateStart= true, errDateEnd= true
+        let errMsgTitle= '', errMsgDates= '' 
+        console.log(title);console.log(startDate, endDate)
+        if (!title || validateWhtieSpaceOnly(title) ) {
+            errMsgTitle = '\n- Please enter a trip name'
+        } else {
+            errLoc= false
+        }
+        if (!(startDate && endDate)) {
+            errDates = "\n- Please select start and end date"
+        } else if (startDate.localeCompare(endDate)>0) {
+            errDates = "\n- Dates are invalid"
+        } else {
+            errDateStart= false
+            errDateEnd= false
+        }
+        console.log(errTitle, errDateStart, errDateEnd)
+        if (errLoc || errDateStart || errDateEnd) {
+            Alert.alert("Hang on!"+errMsgTitle+errMsgDates)
+            return
+        }
+        //console.log("ok?");return
+        _getToken().then(token=>{
+            
+            fetch(serverURL+'/api/trips/edit/'+this.props.selectOnEdit.id,{
+                method: 'PUT',
+                headers: { 
+                    'Authorization': `Bearer ${token}`,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "title": title.trim(), "startDate": startDate, "endDate": endDate,
+                }),
+            }).then(res=>{
+                if (res.status===200) {
+                    res.json().then(data=>{
+                        console.log("edit trips: ")
+                        console.log(data)
+                        this.props.editTrip(data)
+                    })
+                }
+            }).catch(err=>{ console.log(err) })
+        }).catch(error=>{
+            console.log("get token error:",error)
+        })
+    }
+
+    onFocus = () => { this.setState({errTripTitle: false}) }
+
+    onBlur = (evt) => {
+        if (!evt.nativeEvent.text) {
+            this.setState({errTripTitle: true})
+            return
+        }
+    }
+
+    onDateChange = (date, type) => {
+        console.log(type)
+        console.log(dateToString(date))
+        if (type === 'END_DATE') {
+          this.setState({ dateEnd: dateToString(date),})
+        } else {
+          this.setState({ dateStart: dateToString(date),dateEnd: ''})
+        }
+    }
+
+    render() {
+        return (
+    <React.Fragment>
+        
+      <Appbar.Header statusBarHeight={20} style={styles.appbarHeader}>
+        <Appbar.Content title="New Location Info" titleStyle={styles.contentTitle} />
+        <Button onPress={()=>this.props.setModalEdit(false,"modalEditTrip","")}
+            style={{alignItems: 'center', alignContent: 'flex-end'}}>
+            <Text style={{color: "rgb(255,255,255)"}}>Close</Text>
+        </Button> 
+      </Appbar.Header>
+
+      <ScrollView>
+        
+      <TextInput label='Enter Trip Title' mode="outlined" value={this.state.tripTitle}
+            defaultValue={this.state.tripTitle}
+            onChangeText={text => this.setState({ tripTitle: text })}
+            onBlur={this.onBlur} onFocus={this.onFocus}
+            style={{margin: 20, borderRadius: 5, backgroundColor: 'rgb(255,255,255)' }} 
+            error={this.state.errTripTitle}
+        />
+
+      <View >
+        <CalendarPicker
+          startFromMonday={true}
+          allowRangeSelection={true}
+          todayBackgroundColor="#f2e6ff"
+          selectedDayColor="#7300e6"
+          selectedDayTextColor="#FFFFFF"
+          onDateChange={this.onDateChange}
+        />
+ 
+        <View>
+          <Text>SELECTED START DATE:{ this.state.dateStart }</Text>
+          <Text>SELECTED END DATE:{ this.state.dateEnd }</Text>
+        </View>
+      </View>
+
+        <TouchableOpacity onPress={this.submitEditTripInfo} style={modalStyles["create_btn"]}>
+            <Text style={modalStyles["create_btn_text"]}>
+                Update Trip Info
+            </Text>
+        </TouchableOpacity>
+        
+      </ScrollView>
+        
+    </React.Fragment>
+        )
+    }
+}
---
+++ TripTracker/src/Favorites.js
@@ -0,0 +1,323 @@
+import React, { Component } from 'react';
+import { AsyncStorage, StyleSheet, View, Text, ScrollView, Modal, Alert, Image, 
+    TouchableHighlight, TouchableWithoutFeedback, TouchableOpacity, } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import { Appbar, Button, TextInput, Card, Title, Paragraph, Searchbar, TouchableRipple } from  'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
+import { SwipeListView } from 'react-native-swipe-list-view';
+
+import { serverURL, tokenName, } from './config/envConst';
+
+/**
+ * STYLESHEETS
+ */
+const styles = StyleSheet.create({
+    appbarHeader:{
+        backgroundColor: 'rgb(36,152,219)',
+    },
+    contentTitle: {
+        fontSize: 24, fontFamily: 'Avenir',
+    },
+    tripInfo: {
+        marginTop: 5,
+        textAlign: 'center',
+        fontSize: 20, fontFamily: 'Avenir',
+    },
+    addNewBtn: {
+        marginTop: 10 , marginLeft: '25%', 
+        borderRadius: 20, width: '50%', 
+        backgroundColor: 'rgb(49,90,158)'
+    },
+    rowFront: {
+		alignItems: 'flex-start',
+		backgroundColor: 'rgb(255,255,255)',
+		borderBottomColor: 'silver',
+		borderBottomWidth: 1,
+		justifyContent: 'center',
+        height: 100,
+        padding: 10,
+	},
+	rowBack: {
+		alignItems: 'center',
+		backgroundColor: 'transparent',
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingLeft: 15,
+    },
+})
+
+const cardStyles = StyleSheet.create({
+    container: { flex: 1, },
+    content: { padding: 4, },
+    card: { 
+        margin: 10, borderRadius: 10,
+        borderColor: 'rgba(192,192,192,0.75)', borderWidth: 2,
+    },
+    cardImg: {
+        width: 50, height: 50,
+    },
+})
+
+function getDaysDiffText(startDateText,endDateText) {
+    var timeDiff = new Date(endDateText) - new Date(startDateText)
+    var dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24))+1
+    if (dayDiff>1) {
+        return dayDiff+' days'
+    }
+    return dayDiff+' day'
+}
+
+_getToken = async() => { return await AsyncStorage.getItem(tokenName) }
+
+
+/**
+ * FAVORITES
+ */
+export default class Favorites extends Component {
+
+    constructor(props) {
+        super(props)
+        this.focusListener = this.props.navigation.addListener('didFocus',payload => {
+            console.debug('didFocus', payload);
+            this._getFavTripInfo() 
+          })
+        this.state= {
+            selectOnDelete: {}, selectOnEdit: {}, trips: [],
+            modalVisible: false, modalDelete: false,
+        }
+    }
+
+    static navigationOptions = {
+        title: 'Trips',
+        headerTintColor: '#fafafa',
+        headerTitleStyle: {
+            fontSize: 24, fontFamily: 'Avenir',
+        },
+        headerStyle: {
+            backgroundColor: 'rgba(36,152,219,0.85)',
+        }
+    }
+
+    componentDidMount = () => { 
+        //this._getFavTripInfo() 
+    }
+
+    componentWillUnmount = () => { this.focusListener.remove() }
+
+    _signOutAsync = async () => {
+        await AsyncStorage.removeItem(tokenName)
+        this.props.navigation.navigate('Splash')
+    }
+
+    _getFavTripInfo = () => {
+        _getToken().then(token=>{
+            console.log("fav trip container= "+token)
+            fetch(serverURL+'/api/users/favorite',{
+                method: 'GET',
+                headers: { 'Authorization': `Bearer ${token}` }
+            }).then(res=>{
+                if (res.status===200) {
+                    res.json().then(data=>{
+                        console.log(data)
+                        this.setState({ trips: data, })
+                        console.log("done getting fav trips...")
+                    })
+                } else {
+                    res.json().then(data=>{
+                        if (data.message.indexOf('jwt expired')!==-1) {
+                            this._signOutAsync()
+                        }
+                    })
+                }
+                
+            }).catch(err=>{ console.log(err) })
+        }).catch(error=>{
+            console.log("get token error:",error)
+        })
+    }
+
+    addTrips = (trip) => {
+        let trips = this.state.trips
+        trips.push(trip)
+        this.setState({ trips })
+    }
+
+
+    setModalVisible = (visible) => { this.setState({modalVisible: visible}) }
+
+    setModalDelete = (visible) => { this.setState({modalDelete: visible}) }
+    
+    favToggle = (index) => {
+        console.log(this.state.trips[index])
+        _getToken().then(token=>{
+            
+            fetch(serverURL+'/api/trips/edit/'+this.state.trips[index].id,{
+                method: 'PUT',
+                headers: { 
+                    'Authorization': `Bearer ${token}`,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "isFav": !this.state.trips[index].isFav, 
+                }),
+            }).then(res=>{
+                if (res.status===200) {
+                    res.json().then(data=>{
+                        console.log("edit trips isFav: ")
+                        console.log(data)
+                        let filteredTrips = this.state.trips.filter(trip=>{
+                            return (trip.id!==data.id)
+                        })
+                        this.setState({ trips: filteredTrips, })
+                    })
+                }
+            }).catch(err=>{ console.log(err) })
+        }).catch(error=>{
+            console.log("get token error:",error)
+        })
+    }
+
+    toTripDetails = (index) => {
+        this.props.navigation.navigate('TripDetail',{
+            tripId: this.state.trips[index].id, title: this.state.trips[index].title,
+        })
+    }
+
+    _onDeleteTrip = (index) => {
+        this.setState({
+            selectOnDelete: this.state.trips[index], 
+            modalDelete: true,
+        })
+    }
+
+    deleteTrip = (index) => {
+        console.log(this.state.trips[index])
+        let tripId = this.state.trips[index].id
+        this._getToken().then(token=>{
+            fetch(serverURL+'/api/trips/delete/'+tripId,{
+                method: 'DELETE',
+                headers: { 'Authorization': `Bearer ${token}` }
+            }).then(res=>{
+                if (res.status===200) {
+                    res.json().then(data=>{
+                        console.log(data)
+                        let filteredTrips = this.state.trips.filter(trip=>{
+                            return (trip.id!==data.id)
+                        })
+                        this.setState({ trips: filteredTrips, })
+                    })
+                } 
+                    
+            }).catch(err=>{ console.log(err) })
+
+        }).catch(error=>{
+            console.log("get token error:",error)
+        })
+    }
+
+    _removeTrip = (data) => {
+        let filteredTrips = this.state.trips.filter(trip=>{
+            return (trip.id!==data.id)
+        })
+        this.setState({
+            trips: filteredTrips,
+            modalDelete: false,
+        })
+    }
+
+    render() {
+
+        let numTrips = this.state.trips.length>1 ?
+            <Text style={styles.tripInfo}>{`${this.state.trips.length} trips found`}</Text> : 
+            <Text style={styles.tripInfo}>{`${this.state.trips.length} trip found`}</Text>
+        
+        let allTrips = []
+        this.state.trips.map((trip,index)=>{
+            return allTrips.push({key: index.toString(), trip: trip})
+        })
+        console.log(allTrips)
+        
+        return (
+    <React.Fragment>
+        
+        <Appbar.Header statusBarHeight={20} style={styles.appbarHeader}>
+            <Appbar.Content title="Favorites" titleStyle={styles.contentTitle} />
+        </Appbar.Header>
+
+        <View style={{justifyContent: 'space-between', padding: 10, flexDirection: 'row',
+            borderBottomWidth: 1, borderBottomColor: 'silver', }}>
+          
+          {numTrips}          
+          
+          <Button style={{backgroundColor: 'rgb(49,90,158)', borderRadius: 20}} icon="add" mode="contained"
+            onPress={()=>this.setModalVisible(true)}>
+            <Text style={{textAlign: 'center', color: 'rgb(255,255,255)', fontSize: 14}}>
+              Trip
+            </Text>  
+          </Button>
+        </View>
+
+        <SwipeListView
+            useFlatList
+            data={allTrips}
+            //disableRightSwipe={true}
+            renderItem={ (data, rowMap) => {
+                let index= parseInt(data.item.key), trip = data.item.trip
+                return(
+                <TouchableHighlight style={styles.rowFront}
+                    onPress={()=>this.toTripDetails(index)}>
+                  <View>
+                    <Text style={{fontSize: 20, fontFamily: 'Avenir', marginTop: 10}}>
+                        {trip.title.toUpperCase()}
+                      </Text>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                      <Image source={{ uri: serverURL+'/'+trip.image }} style={cardStyles.cardImg} />
+                      
+                      <View style={{marginLeft: 10, alignSelf: 'flex-end', flexDirection: 'column'}}>
+                        <Paragraph style={{fontSize: 14, fontFamily: 'Avenir', margin: 0}}>
+                            Duration: 
+                            <Text style={{color: 'rgb(49,90,158)'}}>
+                                {' '+getDaysDiffText(trip.startDate,trip.endDate)}
+                            </Text>
+                        </Paragraph>
+                        <Paragraph style={{fontSize: 14, fontFamily: 'Avenir'}}>
+                            From: 
+                            <Text style={{color: 'rgb(49,90,158)'}}>
+                                {' '+trip.startDate.split('-').join('/')+' '}
+                            </Text> 
+                            to 
+                            <Text style={{color: 'rgb(49,90,158)'}}>
+                                {' '+trip.endDate.split('-').join('/')}
+                            </Text> 
+                        </Paragraph>
+                      </View>
+                    
+                    </View>
+                  </View>  
+                </TouchableHighlight >)
+            }}
+            renderHiddenItem={ (data, rowMap) => {
+                let index = parseInt(data.item.key)
+                let isFav = data.item.trip.isFav
+                return(
+                <View style={styles.rowBack}>
+                    <Button style={{borderRadius: 5 }} icon={isFav? 'star': ''}
+                        onPress={()=>this.favToggle(index)}>
+                        <Text>{isFav? 'Remove':'Add to favorite'}</Text>
+                    </Button>
+                    <Button style={{borderRadius: 5, }} 
+                        onPress={()=>this._onDeleteTrip(index)}>
+                        <Text style={{color: 'rgb(255,0,0)'}}>Delete</Text>
+                    </Button>
+                </View>
+            )}}
+            leftOpenValue={160}
+            rightOpenValue={-80}
+        />
+
+    </React.Fragment>
+        )
+    }
+}
