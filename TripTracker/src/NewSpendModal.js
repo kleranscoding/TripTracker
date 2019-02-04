b4d8163d24ccbdcbfea52a105ac65fefc991c9b6
kleranscoding <clarencecc88@gmail.com>
2019-02-03 22:58:49 -0600

    styled new spending modal

--- TripTracker/src/NewSpendModal.js
+++ TripTracker/src/NewSpendModal.js
@@ -75,6 +75,7 @@ export default class NewSpendModal extends Component {
             catLabel: '',
             currencyList: currencyInfo,
             resizeCurr: false, resizeCat: false,
+            openCalendar: false
         }
     }
 
@@ -143,7 +144,11 @@ export default class NewSpendModal extends Component {
         })
     }
 
-    onDateChange = (date, type) => { this.setState({ date: dateToString(date),}) }
+    onDateChange = (date, type) => { 
+        this.setState({ 
+            date: dateToString(date), openCalendar: false,
+        }) 
+    }
 
     resizeSelector = (name) => {
         if (this.state[name]) {
@@ -177,7 +182,7 @@ export default class NewSpendModal extends Component {
             <View key={index} style={{alignSelf: 'flex-start'}}>
                 <Button icon={cat.icon} ref={"cat_"+index}
                     onPress={()=>this.selectCategory(index)}>
-                    <Text style={{color: 'rgb(255,255,255)', textAlign: 'left', padding: 5, fontSize: 18}}>
+                    <Text style={{color: 'rgb(36,152,219)', textAlign: 'left', padding: 5, fontSize: 18}}>
                         {this.state.catLabels[index]}
                     </Text>
                 </Button>
@@ -204,10 +209,9 @@ export default class NewSpendModal extends Component {
         </Text>
       </View>
       
-      <ScrollView >
-      <KeyboardAwareScrollView style={{marginBottom: 100}}>
+      <KeyboardAwareScrollView>
         
-        <View >
+        {/* <View >
             <CalendarPicker
                 startFromMonday={true}
                 allowRangeSelection={false}
@@ -220,27 +224,54 @@ export default class NewSpendModal extends Component {
             <View>
             <Text>SELECTED DATE:{ this.state.date }</Text>
             </View>
+        </View> */}
+
+        <View style={{margin: 10}}>
+            <Text style={{margin: 10, fontSize: 16, fontFamily: 'Avenir', color: 'rgb(49,90,158)'}}>
+                DATE: {' '+this.state.date }
+            </Text>
         </View>
+
+        <TouchableOpacity style={{
+                margin: 10, backgroundColor: 'rgb(36,152,219)',
+                borderColor: 'silver', borderWidth: 1, borderRadius: 10, padding: 5
+            }}
+            onPress={()=>this.setState({openCalendar: !this.state.openCalendar})}>
+            <Text style={{
+                textAlign: 'center', fontSize: 20, fontFamily: 'Avenir', color: 'rgb(255,255,255)'}}
+            >
+                {this.state.openCalendar? 'Close ' : 'Open '} Calendar
+            </Text>
+        </TouchableOpacity>
+        
+        {this.state.openCalendar && 
+        <View style={{borderColor: 'silver', borderWidth: 1, margin: 10, borderRadius: 10}}>
+            <CalendarPicker
+                startFromMonday={true}
+                todayBackgroundColor="rgb(49,90,158)"
+                selectedDayColor="rgb(49,90,158)"
+                selectedDayTextColor="rgb(255,255,255)"
+                onDateChange={this.onDateChange}
+            />
+        </View>}
 
         <TextInput label='What did you spend on?' mode="outlined" 
             onChangeText={text => this.setState({ name: text })}
-            style={{margin: 10, borderRadius: 10, backgroundColor: 'rgb(255,255,255)' }} />
+            style={{margin: 10, borderRadius: 10, backgroundColor: 'rgb(255,255,255)', fontFamily: 'Avenir' }} />
         
-        <View style={{flexDirection: this.state.resizeCurr? 'column' : 'row', alignContent: 'center', }}>
-          <View style={{width: this.state.resizeCurr? '100%' : '40%', padding: 10, flex: 1, backgroundColor: 'rgb(49,90,158)', margin: 5}}>
-            <TouchableOpacity onPress={this.resizeCurrency}>
-                <Text style={{color: 'rgb(255,255,255)', fontSize: 18, fontFamily: 'Avenir'}}>
-                    Currency: {this.state.currency+" selected"}
+        <View style={{flexDirection: this.state.resizeCurr? 'column' : 'row', alignContent: 'center', margin: 5}}>
+          <View style={{width: this.state.resizeCurr? '100%' : '42%', justifyContent: 'center', }}>
+            <TouchableOpacity onPress={this.resizeCurrency} 
+                style={{backgroundColor: 'rgb(36,152,219)', borderRadius: 10, padding: 5, margin: 5 }}>
+                <Text style={{textAlign: 'center',color: 'rgb(255,255,255)', fontSize: 18, fontFamily: 'Avenir'}}>
+                    Selected: {this.state.currency}
                 </Text>
             </TouchableOpacity>
             {this.state.resizeCurr && 
                 <Picker style={{backgroundColor: 'rgb(255,255,255)'}}
                     selectedValue={this.state.currency} mode="dropdown"
                     onValueChange={(itemVal, itemIndex) => {
-                        this.setState({
-                            currency: itemVal,
-                            resizeCurr: false, 
-                        })
+                        this.setState({currency: itemVal, resizeCurr: false })
                 }}>
                     {currencyGrp}
                 </Picker>}
@@ -248,35 +279,50 @@ export default class NewSpendModal extends Component {
           <View style={{width: this.state.resizeCurr? '100%' : '50%'}}>
             <TextInput label='How much is that?' mode="outlined" keyboardType='numeric'
               onChangeText={text => this.setState({ amount: text })}
-              style={{margin: 10, borderRadius: 10, backgroundColor: 'rgb(255,255,255)' }} />
+              style={{margin: 10, borderRadius: 10, backgroundColor: 'rgb(255,255,255)', fontFamily: 'Avenir' }} />
           </View>
         </View>
         
         
-        <View style={{padding: 20, flex: 1, backgroundColor: 'rgb(49,90,158)', margin: 20}}>
-            <TouchableOpacity onPress={this.resizeCat} style={{backgroundColor: 'rgba(49,90,158,0.5)'}}>
+        <View style={{padding: 10, backgroundColor: 'rgb(36,152,219)', margin: 20, borderRadius: 10}}>
+            <TouchableOpacity onPress={this.resizeCat} >
                 <Text style={{color: 'rgb(255,255,255)', fontSize: 18, fontFamily: 'Avenir'}}>
                     {this.state.resizeCat? 'Select a category' : 
                         this.state.category!=='' ? 
                         'Category: '+this.state.category+' selected': 'Select a category'}
                 </Text>
             </TouchableOpacity>
-            {this.state.resizeCat && catButtonGrp}
+            {this.state.resizeCat && 
+                <View style={{backgroundColor: 'rgb(255,255,255)', borderWidth: 1, borderColor: 'silver'}}>
+                    {catButtonGrp}
+                </View>}
  
         </View>
         
         <TextInput label='A little more about it maybe? (optional)' mode="outlined" multiline={true}
             onChangeText={text => this.setState({ note: text })}
-            style={{margin: 20, borderRadius: 10, backgroundColor: 'rgb(255,255,255)' }} />
+            style={{margin: 20, borderRadius: 10, backgroundColor: 'rgb(255,255,255)', fontFamily: 'Avenir' }} />
     
-        <TouchableOpacity onPress={this.submitSpendInfo} style={modalStyles["create_btn"]}>
+        {/* <TouchableOpacity onPress={this.submitSpendInfo} style={modalStyles["create_btn"]}>
             <Text style={modalStyles["create_btn_text"]}>
                 Add Expense
             </Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
+
+        <View style={{flexDirection: 'row', justifyContent: 'center', marginTop: 50, marginBottom: 100}}>
+            <Button onPress={()=>this.props.setModalVisible(false)} 
+                style={{marginLeft: 10, marginRight: 10, borderColor: 'silver', borderWidth: 1, borderRadius: 10}}
+            >
+                <Text>Cancel</Text>
+            </Button>
+            <Button onPress={this.submitSpendInfo} 
+                style={{backgroundColor: 'rgb(49,90,158)', borderRadius: 10, marginLeft: 10, marginRight: 10 }}>
+                <Text style={{color: 'white'}}>Add Expense</Text>
+            </Button>
+        </View>
 
       </KeyboardAwareScrollView>
-    </ScrollView>    
+   
     </React.Fragment>
         )
     }
